e0184011befd47a0a56299f87276dc659f449fc9
Kevin <kpie314@gmail.com>
2015-10-09 12:43:50 -0400

    Looping

--- SamplePy/looping.py
+++ SamplePy/looping.py
@@ -1,11 +1,32 @@
-import time
-# For is the simplest of loops. 
-for(k in range(1,5)): # range(1,5) returns the set [1,2,3,4,5] 
-	print("say "+str(k) )#Repeated action where k first = 1
-	time.sleep(0.6) # dramatic pause
-# The Variable k is created for the loop, 
-# iterating through 5 repetitions of the 
-# indented code with  k equaling 1 for the 
-# first run through than 2 than 3 and so on.
+# python begins from the top.
+
+print("The for loop:")		
+for k in range(1,6):#  k is initiated as a temporary variable for each element in range(1,6) [1,2,3,4,5]
+	raw_input("")
+	print("\tsay "+str(k)) # The repeated action is encapsulated by :Curly Brackets 
+#end for
+raw_input("Press enter to continue...")
+print("	The while loop:")
 
 
+k = 0			# Here we create the variable k before the loop (it belongs to the main method) 
+while(k < 5):
+	raw_input("")
+	print("\toh "+str(k))# Again we have an action repeated
+	k+=1
+#end while
+raw_input("Press enter to continue...")
+print("Recursion:")
+
+
+def recPrint(n):# Here we define the recrsive method
+	raw_input("")
+	print("\tk? "+str(n))
+	if(n>0):
+		recPrint(n-1)
+	#end if
+#end recPrint
+recPrint(5)			#And here we use the recursive method.
+
+raw_input("Press enter to continue...")
+print("You have now seen the 3 fundemental kinds of loops.")
